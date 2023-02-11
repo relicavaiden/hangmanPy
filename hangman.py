6f13e81f7932e77104beead5e5f586e50f9cd3cf
relicavaiden <dirrickricks@gmail.com>
2023-02-11 11:44:45 -0600

    Hangman game in python

---
+++ hangman.py
@@ -0,0 +1,45 @@
+import random
+from words import words 
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def winner():
+    print("Congratulations you have won!")
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # What has been guessed already
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    # get user input
+    while len(word_letters) > 0:
+
+        # Letters guessed
+        print('You have used these letters: ', ' '.join(used_letters))
+
+        # What the current word is with stars
+        word_list = [letter if letter in used_letters else '*' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already choosen this letter. Please choose again.')
+
+        else:
+            print('Please use only letters A to Z. No numbers or characters.')
+
+     
+
+hangman()
+winner()
