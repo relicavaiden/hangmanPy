c0f29f96a66d4bdaa0aa32900f69d7a70c30248b
relicavaiden <dirrickricks@gmail.com>
2023-02-12 16:19:46 -0600

    added lives counter

--- hangman.py
+++ hangman.py
@@ -9,20 +9,19 @@ def get_valid_word(words):
 
     return word.upper()
 
-def winner():
-    print("Congratulations you have won!")
-
 def hangman():
     word = get_valid_word(words)
     word_letters = set(word) # What has been guessed already
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
+    lives = 6
+
     # get user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
 
         # Letters guessed
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         # What the current word is with stars
         word_list = [letter if letter in used_letters else '*' for letter in word]
@@ -33,13 +32,21 @@ def hangman():
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
 
+            else:
+                lives = lives - 1 # minus one live each time that you make a mistake
+                print('This letter is not in the word')
+
         elif user_letter in used_letters:
             print('You have already choosen this letter. Please choose again.')
 
         else:
             print('Please use only letters A to Z. No numbers or characters.')
 
+    if lives == 0:
+        print('Sorry you lost, the word was:', word)
+    else:
+        print('Congratulations you guessed the word', word, 'correctly!!')
+
      
 
 hangman()
-winner()
